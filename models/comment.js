753d89ae7d04a52b7efe8ce804bfa5550a2f13a6
Victoria <vmcnorrill@gmail.com>
2023-04-25 20:31:09 -0400

    cerated models for blogpost, comment

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+     },
+     comment: {
+        type: DataTypes.STRING,
+        allowNull: false,
+     },
+     user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+     },
+     date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+     },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+})
+
 module.exports = Comment;
