d1931441271ceabf1ce135c64ae474b088bfb2c4
Victoria <vmcnorrill@gmail.com>
2023-04-26 19:32:54 -0400

    writing routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,68 @@
 const router = require('express').Router();
 const { User, BlogPost, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
+// HOMEPAGE GET REQUEST
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all blog posts and JOIN with user data
+      const blogpostData = await BlogPost.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const blogPosts = blogpostData.map((BlogPost) => BlogPost.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        blogPosts, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // GET one blogpost with id
+router.get('/blogpost/:id', withAuth, async (req, res) => {
+    try {
+      const blogpostData = await BlogPost.findByPk(req.params.id, {
+        include: [
+          {
+            model: comment,
+            attributes: [
+              'id',
+              'comment',
+              'date_created',
+              'user_id',
+              'blogpost_id',
+            ],
+          },
+        ],
+      });
+      const specificBlogPosts = BlogPost.get({ plain: true });
+      res.render('blogposts', { specificBlogPosts, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+
+
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
