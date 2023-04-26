be40dc27e4c52d26e9b981ea2561efb58cab7f86
Victoria <vmcnorrill@gmail.com>
2023-04-25 20:41:20 -0400

    added joins for models

--- models/index.js
+++ models/index.js
@@ -2,5 +2,34 @@ const User = require('./user')
 const BlogPost = require('./blogpost')
 const Comment = require('./comment')
 
+//USER HAS MANY POSTS
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+BlogPost.belongsTo(User, {
+foreignKey: 'user_id'
+});
+
+//USER HAS MANY COMMENTS
+User.hasMany(Comment, {
+foreignKey: 'user_id',
+onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+foreignKey: 'user_id'
+});
+
+//BLOGPOST HAS MANY COMMENTS
+BlogPost.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+    });
+    
+Comment.belongsTo(BlogPost, {
+foreignKey: 'user_id'
+});
 
 module.exports = { User, BlogPost, Comment };
