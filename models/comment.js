919c6c3130f9df58dc0abd8e0244066e3e48246e
Victoria <vmcnorrill@gmail.com>
2023-04-27 14:16:41 -0400

    debugged seeds

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+class Comment extends Model {}
+
 Comment.init({
     id: {
         type: DataTypes.INTEGER,
--- seeds/index.js
+++ seeds/index.js
@@ -1,16 +1,18 @@
-// const sequelize = require('../config/connection');
-// const { User, Project } = require('../models');
+const sequelize = require('../config/connection');
+const { User, Comment, BlogPost } = require('../models');
 
-// const userData = require('./userData.json');
-// const projectData = require('./projectData.json');
+const userData = require('./userData.json');
+const blogPostData = require('./blogData.json');
+const commentData = require('./commentData.json')
 
-// const seedAll = async () => {
-//   await sequelize.sync({ force: true });
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
 
-//   const users = await User.bulkCreate(userData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
+//   const users = 
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
 //   for (const project of projectData) {
 //     await Project.create({
@@ -19,7 +21,7 @@
 //     });
 //   }
 
-//   process.exit(0);
-// };
+  process.exit(0);
+};
 
-// seedAll();
+seedAll();
