581fd7de03778056adf6e464fd405016d1f3aa66
Victoria <vmcnorrill@gmail.com>
2023-06-03 16:09:06 -0400

    reconfigured connection

--- controllers/api/user-Routes.js
+++ controllers/api/user-Routes.js
@@ -14,6 +14,10 @@ router.get("/", async (req, res)=>{
 // CREATE new user - Sign up
 //api/users/signup
 
+router.get(
+  
+)
+
 router.post('/signup', async (req, res) => {
   try {
     const dbUserData = await User.create({
