6fe4988c78cf97f1df8fbddd7c13a23ecdb706fe
Victoria <vmcnorrill@gmail.com>
2023-06-03 20:33:52 -0400

    changes

--- utils/password.js
+++ utils/password.js
@@ -1,23 +1,20 @@
 function passwordSignUpFunction() {
   let passwordInput = document.getElementById("password-signup");
-  
-  for (let i = 0; i < passwordInput.length; i++) {
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-    } else {
-      passwordInput.type = "password";
-    }
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
   }
+
 }
 
 function passwordLoginFunction() {
   let passwordInput = document.getElementById("password-login");
 
-  for (let i = 0; i < passwordInput.length; i++) {
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-    } else {
-      passwordInput.type = "password";
-    }
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
   }
 }
