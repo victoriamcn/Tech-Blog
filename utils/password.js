41e7b6f12ba39a90b30aab20f9a7120028424ef6
Victoria <vmcnorrill@gmail.com>
2023-06-03 20:08:41 -0400

    deleted password toggle, wasn't working

--- utils/password.js
+++ utils/password.js
@@ -1,5 +1,18 @@
-function passwordFunction() {
-  let passwordInput = document.querySelectorAll(".password-input");
+function passwordSignUpFunction() {
+  let passwordInput = document.getElementById("password-signup");
+  
+  for (let i = 0; i < passwordInput.length; i++) {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+    } else {
+      passwordInput.type = "password";
+    }
+  }
+}
+
+function passwordLoginFunction() {
+  let passwordInput = document.getElementById("password-login");
+
   for (let i = 0; i < passwordInput.length; i++) {
     if (passwordInput.type === "password") {
       passwordInput.type = "text";
